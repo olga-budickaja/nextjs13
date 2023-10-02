89eff190badf1a933eefe5affb4521ab15d724be
Olga Buditska <olgabudickaja@gmail.com>
2023-10-02 17:25:12 +0300

    created LocalSearchbar

---
+++ components/shared/search/LocalSearchbar.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import { Input } from '@/components/ui/input'
+import Image from '@/node_modules/next/image'
+
+interface CustomInputProps {
+    route: string;
+    iconPosition: string;
+    imgSrc: string;
+    placeholder: string;
+    otherClasses?: string;
+}
+
+const LocalSearchbar = ({ route, iconPosition, imgSrc, placeholder, otherClasses }: CustomInputProps) => {
+  return (
+      <div className={`background-light800_darkgradient flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${otherClasses && otherClasses}`}>
+        {iconPosition === 'left' && (
+            <Image
+                src={imgSrc}
+                alt="Search"
+                width={24}
+                height={24}
+                className='cursor-pointer'
+            />
+        )}
+
+        <Input
+            type="text"
+            placeholder={placeholder}
+            value=""
+            className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
+            onChange={() => {}}
+        />
+
+        {iconPosition === 'right' && (
+            <Image
+                src={imgSrc}
+                alt="Search"
+                width={24}
+                height={24}
+                className='cursor-pointer'
+            />
+        )}
+      </div>
+  )
+}
+
+export default LocalSearchbar

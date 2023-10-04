adb4ba094ba93157662dd2ed70f24dbc02db448b
Olga Buditska <olgabudickaja@gmail.com>
2023-10-04 13:14:57 +0300

    added question page

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,9 +1,13 @@
+import Question from '@/components/forms/Question'
 import React from 'react'
 
 const AskQuestion = () => {
   return (
     <div>
-      AskQuestion
+      <h1 className='h1-bold text-dark100_light900'>Ask a public quesiton</h1>
+      <div className='mt-9'>
+        <Question />
+      </div>
     </div>
   )
 }

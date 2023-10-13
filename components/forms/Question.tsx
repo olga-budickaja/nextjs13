13af7b52eba25247aebc8772e1cd887092dbd967
Olga Buditska <olgabudickaja@gmail.com>
2023-10-13 14:53:45 +0300

    added answers form

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -3,7 +3,7 @@
 import React, { useRef, useState } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
 import { zodResolver } from "@hookform/resolvers/zod"
-import * as z from "@/node_modules/zod/lib/index"
+import { z } from 'zod';
 import {
     Form,
     FormControl,
@@ -21,6 +21,7 @@ import { Badge } from '../ui/badge';
 import Image from '@/node_modules/next/image';
 import { createQuestion } from '@/lib/actions/question.action';
 import { usePathname, useRouter } from '@/node_modules/next/navigation';
+import { useTheme } from '@/context/ThemeProvider';
 
 const type: any = 'create';
 
@@ -29,6 +30,7 @@ interface Props {
 }
 
 const Question = ({ mongoUserId }: Props) => {
+    const { mode } = useTheme();
     const editorRef = useRef(null);
     const [isSubmitting, setIsSubmitting] = useState(false);
 
@@ -161,7 +163,9 @@ const Question = ({ mongoUserId }: Props) => {
                 'undo redo | ' +
                 'codesample | bold italic forecolor | alignleft aligncenter |' +
                 'alignright alignjustify | bullist numlist',
-                content_style: 'body { font-family:Inter; font-size:16px }'
+                content_style: 'body { font-family:Inter; font-size:16px }',
+                skin: mode === 'dark' ? 'oxide-dark' : 'oxide',
+                content_css: mode === 'dark' ? 'dark' : 'light',
                 }}
             />
             </FormControl>
